d5441b252f2c72f4cc3cab50b934960e6cbfbc76
koki2254 <koki.9635225@gmail.com>
2023-11-13 10:36:09 +0900

    ログイン画面#1

---
+++ MyGitProject2a/src/main/java/controller/gitLoginController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class gitLoginController {
+	@RequestMapping(path = "/gitpralogin", method = RequestMethod.GET)
+	public String homelogin() {
+		return "gitlogin";
+	}
+	
+	@RequestMapping(path = "/gitpralogin", method = RequestMethod.GET)
+	public String homelogin(String ID, String PW, Model model) {
+		model.addAttribute(ID);
+		model.addAttribute(PW);
+		if("gitlogin".equals(ID)&&"gitpw".equals(PW)) {
+			return "redirect:/gititemlist";
+		}else {
+			return "redirect:/gitlogin";
+		}
+	}
+}
