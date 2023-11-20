066ccdd1043af38022a2623232e129612d1178f2
syokapon <is.yuto7777@gmail.com>
2023-11-20 09:57:22 +0900

    Branchテスト

--- MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -1,18 +1,20 @@
 package com.example.demo.controller;
 
-
 import java.util.Random;
 
 public class MergeHellController {
 	public static void main(String[] args) {
 		kadaiA();
+		kadaiB();
 
 	}
-	static void kadaiB(){
+
+	static void kadaiB() {
 		Random a = new Random();
 		int x = a.nextInt(101) + 1;
 		System.out.println(x + "が出ました");
-}
+	}
+
 	static void kadaiA() {
 		for (int i = 1; i < 11; i++) {
 			System.out.println("現在" + i + "回目のループです");
